9511f93d668836ab9f9c747a0ae3c62066ae6094
Anderson Tao <anderson@bstocksolutions.com>
2019-05-30 13:55:46 -0700

    init

---
+++ stories/index.stories.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+
+import { Button, Welcome } from '@storybook/react/demo'
+import { TestComponent } from '../src/index'
+
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+))
+
+storiesOf('Button', module)
+  .add('with text', () => <TestComponent />)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ))
