ef2b3b9ad69f98ba237fc0a2c7ae7ea466a05f38
Anderson Tao <anderson@bstocksolutions.com>
2019-05-30 15:08:51 -0700

    fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,6 @@
-<<<<<<< HEAD
-import babel from 'rollup-plugin-babel'
-
-export default {
-  external: ['react'],
-  input: 'src/index.js',
-  output: {
-    file: 'bundle.js',
-    // Also note 'es' not 'iife', since a library exports something, unlike an application.
-    format: 'es',
-  },
-  plugins: [
-    babel({
-      presets: ['react'],
-    }),
-  ],
-}
-=======
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
-import pkg from './package.json'
 
 export default [
   {
@@ -42,4 +23,3 @@ export default [
     },
   },
 ]
->>>>>>> rollup
